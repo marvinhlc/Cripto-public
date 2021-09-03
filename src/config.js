26559dcccada86ccc0a31b79c03c53a82d88f03f
Marvin H. Linares Caridad <marvinhlc@gmail.com>
2021-09-02 19:42:14 -0600

    Delete config.js

--- src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    URL_API : 'http://localhost:3005/',
-    URL_API_ : 'https://ascav-cripto-api.herokuapp.com/',
-    PAYPAL_CLIENT_ID: "AY8LxFiIVo7VhNN1DU-PzI9iVTk6qKTYTUirAMlIfFLk8bMG6zXSlYu9JwqmEx3SJprYzMPIBvWlSGOo",
-    PAYPAL_SECRET: "EFSJvYT87y62-lqKLXVed-1L5HRz73ORavA-6De8XqEvGmzt-ksfSd8_NkTDPCiPdreV9uRjKxfvXgrv",
-}
